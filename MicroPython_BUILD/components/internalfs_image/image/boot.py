a25ea1aa35770a50b2a5cc465bedb80f303937bb
0x1abin <270995079@qq.com>
2018-03-22 14:50:51 +0800

    updated the I2S module

--- MicroPython_BUILD/components/internalfs_image/image/boot.py
+++ MicroPython_BUILD/components/internalfs_image/image/boot.py
@@ -1,12 +1,7 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-import sys
-sys.path[1] = '/flash/lib'
 
-# ---------- M5Cloud ------------
-if True:
-    from m5stack import lcd, buttonA, buttonB
-    if buttonB.isPressed():
-        lcd.println('On: OFF-LINE Mode', color=lcd.ORANGE)
-    else:
-        import wifisetup
-        import m5cloud
+import sys
+
+# Set default path
+# Needed for importing modules and upip
+sys.path[1] = '/flash/lib'
--- MicroPython_BUILD/components/internalfs_image/image/flash.sh
+++
@@ -1,2 +0,0 @@
-esptool.py --chip esp32 --port /dev/tty.SLAB_USBtoUART --baud 961200 write_flash -z 0x1000 ~/workspace/M5Stack/M5Cloud/firmwares/m5cloud-20180309-v0.3.6.bin
-ampy put SFArch_48.fon && ampy put main.py
