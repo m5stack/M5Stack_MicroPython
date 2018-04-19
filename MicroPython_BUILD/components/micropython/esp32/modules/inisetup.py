eabe28331fc424d3b0e198f6063f492abfcf1fbd
0x1abin <270995079@qq.com>
2018-04-19 09:47:40 +0800

    v0.3.9 res

--- MicroPython_BUILD/components/micropython/esp32/modules/inisetup.py
+++ MicroPython_BUILD/components/micropython/esp32/modules/inisetup.py
@@ -7,10 +7,10 @@ def setup():
 # This file is executed on every boot (including wake-boot from deepsleep)
 import sys
 sys.path[1] = '/flash/lib'
+from m5stack import lcd, speaker, buttonA, buttonB, buttonC
 
 # ---------- M5Cloud ------------
 if True:
-    from m5stack import lcd, buttonA, buttonB
     if buttonB.isPressed():
         lcd.println('On: OFF-LINE Mode', color=lcd.ORANGE)
     else:
